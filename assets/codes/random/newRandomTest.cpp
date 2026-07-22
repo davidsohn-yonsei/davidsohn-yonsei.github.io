9e272f66ded32c595c30c19054f7bd42408b7fc6
Jung Won Sohn <davidsohn@yonsei.ac.kr>
2026-07-22 19:31:29 +0900

    Update newRandomTest.cpp

--- assets/codes/random/newRandomTest.cpp
+++ assets/codes/random/newRandomTest.cpp
@@ -3,44 +3,40 @@
 #include <random>
 
 
-std::mt19937 gen[100'000];
+std::mt19937 gen;
 uint32_t range[100'000];
 
-std::mt19937 gen_dice[100'000];
-uint32_t dice[100'000];
+
+uint32_t newRand(std::mt19937& gen, uint32_t max_val) {
 
 
 
-uint32_t newRand(uint32_t raw_rand, uint32_t max_val) {
-
-    static std::mt19937 bad(1234);
-
-    uint32_t current_rand = raw_rand;
-    uint32_t r = (- static_cast<uint32_t>(max_val)) % max_val;
-
-    while (true) {
 
 
+    uint32_t current_rand = gen();
     uint64_t B = static_cast<uint64_t>(current_rand) * max_val;
 
     uint32_t y = B & 0xFFFFFFFFULL;  // it is same as static_cast<uint32_t>(B)
 
 
 
-    if (y >= max_val || y >= r) {
-        return static_cast<uint32_t>(B >> 32);
-    }
-    // Mostly y >= max_val is true. Even it is false, y >= r then okay.
+    if (y < max_val) {  // Moved while into the if.
 
+        uint32_t r = (-max_val) % max_val;
 
-    current_rand = bad();
+        while (y < r) {
+            current_rand = gen();
+            B = static_cast<uint64_t>(current_rand) * max_val;
+            y = static_cast<uint32_t>(B);
+        }
     }
+    return static_cast<uint32_t>(B >> 32);
 }
 
-uint32_t oldRand(int idx, uint32_t num) {
+uint32_t oldRand(std::mt19937& gen, uint32_t num) {
     static std::uniform_int_distribution<uint32_t> dis;
 
-     return dis(gen[idx], std::uniform_int_distribution<uint32_t>::param_type(0, num-1));
+     return dis(gen, std::uniform_int_distribution<uint32_t>::param_type(0, num-1));
 }
 
 
@@ -53,45 +49,37 @@ int main()
 {
     uint32_t result = 0;
 
+    uint32_t seed = 1234567;  // Just any number.
+    gen.seed(seed);
 
 	for (int k = 0; k < 100'000; k++){
-        uint32_t p = static_cast<uint32_t>(
-            std::chrono::high_resolution_clock::now().time_since_epoch().count()
-	    );
-        uint32_t t1 = p + k; // Adding k just for the cause if same p occurs
-	    if ( (t1 & (t1 - 1)) == 0) {
-                t1 = ~t1;
+
+        uint32_t t = gen();
+	    if ( (t & (t - 1)) == 0) {
+                t = ~t;
 	    }
-	    // If t1 is 2 ^ n, then t1 will become 2^32 - 1 - t1
+	    // If t is 2 ^ n, then t will become 2^32 - 1 - t
 
-	    uint32_t t2 = p + k + 10000;
-        if ((t2 & (t2 - 1)) == 0) t2 = ~t2;
-        gen_dice[k].seed(t2);
+	    range[k] = t;
 
 
-	    gen[k].seed(t1);
-
-	    range[k] = gen[k]();
-
-
-	    dice[k] = gen_dice[k]();
 
 	}
 
 
-
+    gen.seed(seed);
     auto start_old = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < 100'000; i++) {
         // 32 bits random maximum
-        result += oldRand(i, range[i]);
+        result += oldRand(gen, range[i]);
     }
     auto end_old = std::chrono::high_resolution_clock::now();
     auto total_old = std::chrono::duration_cast<std::chrono::nanoseconds>(end_old - start_old).count();
 
-
+    gen.seed(seed);
     auto start_new = std::chrono::high_resolution_clock::now();
     for (int j = 0; j < 100'000; j++) {
-        result += newRand(dice[j], range[j]);
+        result += newRand(gen, range[j]);
     }
     auto end_new = std::chrono::high_resolution_clock::now();
     auto total_new = std::chrono::duration_cast<std::chrono::nanoseconds>(end_new - start_new).count();
